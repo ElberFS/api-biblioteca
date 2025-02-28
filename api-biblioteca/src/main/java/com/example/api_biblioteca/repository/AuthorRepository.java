9640c9a6e51096882e14edf135bec6c9ec40340e
ElberFS <elberjesus09@gmail.com>
2025-02-28 10:57:27 -0500

    Eliminando todo el contenido del repositorio

--- api-biblioteca/src/main/java/com/example/api_biblioteca/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api_biblioteca.repository;
-
-import com.example.api_biblioteca.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- api-biblioteca/src/main/java/com/example/api_biblioteca/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.api_biblioteca.repository;
-
-import com.example.api_biblioteca.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
